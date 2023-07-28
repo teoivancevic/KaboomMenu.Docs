0a2fc0eac57823a8a58b00e71900f5cd48ed0256
Teo Ivančević <teo@tomsoft.hr>
2023-07-28 02:35:52 +0200

    change url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,11 +12,12 @@ const config = {
 
   // Set the production url of your site here
   //url: 'https://teoivancevic.github.io',
-  url: 'https://docs.kaboom.menu',
+  //url: 'https://docs.kaboom.menu',
+  url: 'https://teoivancevic.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-  //baseUrl: '/KaboomMenu.Docs',
+  //baseUrl: '/',
+  baseUrl: '/KaboomMenu.Docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
